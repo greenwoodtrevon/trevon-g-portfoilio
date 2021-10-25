45888b95d5c0a60481ab52fa487ef7f062afbbb9
Trevon Greenwood <greenwoodtrevon@gmail.com>
2021-10-24 19:03:03 -0600

    final commit before submission. Will add more content later.

--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -7,12 +7,24 @@ function MyWork(props) {
 
     <Container className="forms-background">
       <Col>
-        <ul>
+        <ul className='workList'>
           <li>
-            Work 1
+            <a className='contacts' href=' https://eastonshaum.github.io/Join-Coin/'>
+              <h3>JoinCoin</h3>
+              <p>A beginner's way of getting into the world of cryptocurrency.</p>
+            </a>
           </li>
           <li>
-            Work 2
+            <a className='contacts' href='https://offline-library.herokuapp.com/'>
+              <h3>Spotify Offline Library</h3> 
+              <p>As the name suggests, an offline library/vault where your spotify playlists can live.</p>
+            </a>
+          </li>
+          <li>
+            <a className='contacts' href='http://evening-citadel-70565.herokuapp.com/'>
+              <h3>Recipe Book</h3>
+              <p>A blog where users can create and share their own recipes, as well as find recipes from others.</p>
+            </a>
           </li>
         </ul>
       </Col>  
